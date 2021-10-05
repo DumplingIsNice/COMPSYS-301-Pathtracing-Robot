6b771057e8489fef016bf5bff4e73a41ca8f388b
Hao Lin <68680028+uoa-hlin784@users.noreply.github.com>
2021-10-05 13:47:50 +1300

    Update project.h

--- software/simulator/pathfinding_algorithm/cs301_RS1/robot_simulation/project.h
+++ software/simulator/pathfinding_algorithm/cs301_RS1/robot_simulation/project.h
@@ -1,8 +1,29 @@
+/* 
 
+        project.h
+
+        This file includes all declearations and definitions common to other simulation libraries.
+
+        Remember to include c libraries as 
+
+            extern "C" 
+            {
+                #include ...library here...
+            }
+
+*/
+
+#ifndef PROJECT_H
+#define PROJECT_H
+
+/* Enum is used only when specifying a specific direction (i.e. in Navigation). */
 enum Direction {LEFT, RIGHT, FORWARD};
 
+/* Struct is used for passing infomation on currently valid paths (i.e. SensorFSM->MovementController). */
 typedef struct Directions {
- int left;
- int right;
- int forward;
+    int left;
+    int right;
+    int forward;
 } Directions;
+
+#endif // PROJECT_H
